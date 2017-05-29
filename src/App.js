7606d58dee4192b92213a0bb4952688c08bdb586
Joel George V <joel.gv3@gmail.com>
2017-05-30 00:37:17 +0530

    Use single quotes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="content">
+      <div className='content'>
         <Intro />
         <CheckboxTree />
       </div>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,10 +6,10 @@ class Intro extends Component {
       <div>
         <h3>React Checkbox Tree</h3>
         <p>A react app showcasing a simple checkbox tree component. This project was created
-        using <a href="https://github.com/facebookincubator/create-react-app">create-react-app</a>.</p>
-        <p>Uses <a href="https://github.com/rtfeldman/seamless-immutable">seamless-immutable</a> as
+        using <a href='https://github.com/facebookincubator/create-react-app'>create-react-app</a>.</p>
+        <p>Uses <a href='https://github.com/rtfeldman/seamless-immutable'>seamless-immutable</a> as
         the data structure for the component state. Check out the
-        source <a href="https://github.com/joelgeorgev/react-checkbox-tree">here</a>.
+        source <a href='https://github.com/joelgeorgev/react-checkbox-tree'>here</a>.
         </p>
       </div>
     );
