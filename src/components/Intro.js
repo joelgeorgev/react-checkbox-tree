71f82a340f89e49a8a0bfecb34043c1136242ec0
Joel George V <joel.gv3@gmail.com>
2019-01-27 12:53:10 +0000

    Update to remove all immutable library references

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,8 +7,6 @@ export class Intro extends React.PureComponent {
         <h3>React Checkbox Tree</h3>
         <p>A react app showcasing a simple checkbox tree component. This project was created
         using <a href='https://github.com/facebookincubator/create-react-app'>create-react-app</a>.
-        Uses <a href='https://github.com/rtfeldman/seamless-immutable'>seamless-immutable</a> as
-          the data structure for the component state.
         </p>
       </div>
     )
