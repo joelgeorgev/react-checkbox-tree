cde20a27853d388ab72764c5c6fe4eb6d39a4b16
Joel George V <joel.gv3@gmail.com>
2019-08-07 20:10:06 +0100

    Create global style - Remove index.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createGlobalStyle } from 'styled-components'
 
 import { App } from './App'
+import woff2 from './fonts/source-sans-pro-v11-latin-regular.woff2'
+import woff from './fonts/source-sans-pro-v11-latin-regular.woff'
 import registerServiceWorker from './registerServiceWorker'
-import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+
+  /* source-sans-pro-regular - latin */
+  @font-face {
+    font-family: 'Source Sans Pro';
+    font-style: normal;
+    font-weight: 400;
+    font-display: optional;
+    src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'),
+        url(${woff2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+        url(${woff}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+  }
+`
+
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById('root')
+)
 registerServiceWorker()
