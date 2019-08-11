734eb0b2aa34d0aa69df0d5d88b53f95730fda26
Joel George V <joel.gv3@gmail.com>
2019-08-11 23:03:43 +0100

    Fix footer position

--- src/App.js
+++ src/App.js
@@ -11,17 +11,19 @@ const Section = styled.section`
   max-width: 64rem;
   height: 100vh;
   margin: 0 auto;
-  padding: 4rem 0;
 `
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
+  margin-top: 2rem;
+  overflow: auto;
 `
 
 const Footer = styled.div`
   align-self: center;
+  margin: 2rem;
 `
 
 export const App = () => (
