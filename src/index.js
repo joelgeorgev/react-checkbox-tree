d72ad53ce31a49754fd62978741c1fe7ac819751
Joel George V <joel.gv3@gmail.com>
2019-08-12 21:45:37 +0100

    Disable pull-to-refresh and overscroll glow

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-family: 'Source Sans Pro', sans-serif;
+    overscroll-behavior-y: none;
   }
 
   /* source-sans-pro-regular - latin */
