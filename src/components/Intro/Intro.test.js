f15806b1447eefa4afbbe3716f95564d454902bc
Joel George V <joel.gv3@gmail.com>
2019-09-14 23:12:37 +0100

    Add more tests for Intro.js

--- src/components/Intro/Intro.test.js
+++ src/components/Intro/Intro.test.js
@@ -1,13 +1,29 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import { Intro } from '.'
 
 describe('Intro tests', () => {
-  afterEach(cleanup)
+  const getTitle = (node) => node.querySelector('h3').textContent
+  const getDescription = (node) => node.querySelector('p').textContent
+  const getGitHubURL = (node) => node.querySelector('a').href
 
   test('It should render a title', () => {
-    const { getByText } = render(<Intro />)
-    expect(getByText('React Checkbox Tree')).toBeDefined()
+    const { container } = render(<Intro />)
+    expect(getTitle(container)).toEqual('React Checkbox Tree')
+  })
+
+  test('It should render a description', () => {
+    const { container } = render(<Intro />)
+    expect(getDescription(container)).toEqual(
+      'A react app showcasing a simple checkbox tree component. This project was created using create-react-app.'
+    )
+  })
+
+  test('It should render the correct GitHub URL for create-react-app', () => {
+    const { container } = render(<Intro />)
+    expect(getGitHubURL(container)).toEqual(
+      'https://github.com/facebookincubator/create-react-app'
+    )
   })
 })
