a52063ca3e234a95010c0ad985f5dfc10dc27943
Joel George V <joel.gv3@gmail.com>
2019-09-22 23:20:49 +0100

    Remove react-test-renderer and associated test

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import renderer from 'react-test-renderer'
 
 import { App } from './App'
 
 it('smoke test', () => {
   const container = document.createElement('div')
   ReactDOM.render(<App />, container)
-})
-
-it('state change test', () => {
-  const container = document.createElement('div')
-  renderer.act(() => {
-    ReactDOM.render(<App />, container)
-  })
-  const inputElement = container.querySelectorAll('input')[2]
-  renderer.act(() => {
-    inputElement.dispatchEvent(new MouseEvent('click', { bubbles: true }))
-  })
-  expect(inputElement.checked).toBe(true)
 })
