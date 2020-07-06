0b2284a5fd00afb2e147093bcbd1b53294ab2270
Joel George V <joel.gv3@gmail.com>
2020-07-06 19:25:41 +0100

    Refactor tests
     - Simplify inputs and outputs in test cases
     - Use array.forEach to loop thru test cases
     - Remove unnecessary spread operations

--- src/utils/getNewState.test.js
+++ src/utils/getNewState.test.js
@@ -7,85 +7,94 @@ const createNode = (overrides) => ({
   ...overrides
 })
 
-const createParent = () => createNode({ childIds: [1, 2] })
-const createFirstChild = () => createNode({ childIds: [3], parentId: 0 })
-const createSecondChild = () => createNode({ parentId: 0 })
-const createGrandChild = () => createNode({ parentId: 1 })
+const createParent = (overrides) =>
+  createNode({ childIds: [1, 2], ...overrides })
+
+const createFirstChild = (overrides) =>
+  createNode({ childIds: [3], parentId: 0, ...overrides })
+
+const createSecondChild = (overrides) =>
+  createNode({ parentId: 0, ...overrides })
+
+const createGrandChild = (overrides) =>
+  createNode({ parentId: 1, ...overrides })
 
 describe('getNewState', () => {
-  describe('Given a state and an id', () => {
-    const cases = [
-      [
-        // Check node
-        {
-          0: createNode()
-        },
-        0,
-        {
-          0: { ...createNode(), checked: true }
-        }
-      ],
-      [
-        // Uncheck node
-        {
-          0: { ...createNode(), checked: true }
-        },
-        0,
-        {
-          0: createNode()
-        }
-      ],
-      [
-        // Check child node
-        {
-          0: createParent(),
-          1: createFirstChild(),
-          2: createGrandChild(),
-          3: createSecondChild()
-        },
-        3,
-        {
-          0: createParent(),
-          1: createFirstChild(),
-          2: createGrandChild(),
-          3: { ...createSecondChild(), checked: true }
-        }
-      ],
-      [
-        // Check all child nodes recursively
-        {
-          0: createParent(),
-          1: createFirstChild(),
-          2: createGrandChild(),
-          3: createSecondChild()
-        },
-        0,
-        {
-          0: { ...createParent(), checked: true },
-          1: { ...createFirstChild(), checked: true },
-          2: { ...createGrandChild(), checked: true },
-          3: { ...createSecondChild(), checked: true }
-        }
-      ],
-      [
-        // Check all parent nodes recursively
-        {
-          0: createParent(),
-          1: createFirstChild(),
-          2: createGrandChild(),
-          3: { ...createSecondChild(), checked: true }
-        },
-        2,
-        {
-          0: { ...createParent(), checked: true },
-          1: { ...createFirstChild(), checked: true },
-          2: { ...createGrandChild(), checked: true },
-          3: { ...createSecondChild(), checked: true }
-        }
-      ]
-    ]
-    test.each(cases)('returns a new state', (state, id, newState) => {
-      expect(getNewState({ state, id })).toEqual(newState)
+  ;[
+    {
+      prevState: {
+        0: createNode()
+      },
+      id: 0,
+      newState: {
+        0: createNode({ checked: true })
+      }
+    },
+    {
+      prevState: {
+        0: createNode({ checked: true })
+      },
+      id: 0,
+      newState: {
+        0: createNode()
+      }
+    },
+    {
+      prevState: {
+        0: createParent(),
+        1: createFirstChild(),
+        2: createGrandChild(),
+        3: createSecondChild()
+      },
+      id: 3,
+      newState: {
+        0: createParent(),
+        1: createFirstChild(),
+        2: createGrandChild(),
+        3: createSecondChild({ checked: true })
+      }
+    },
+    {
+      prevState: {
+        0: createParent(),
+        1: createFirstChild(),
+        2: createGrandChild(),
+        3: createSecondChild()
+      },
+      id: 0,
+      newState: {
+        0: createParent({ checked: true }),
+        1: createFirstChild({ checked: true }),
+        2: createGrandChild({ checked: true }),
+        3: createSecondChild({ checked: true })
+      }
+    },
+    {
+      prevState: {
+        0: createParent(),
+        1: createFirstChild(),
+        2: createGrandChild(),
+        3: createSecondChild({ checked: true })
+      },
+      id: 2,
+      newState: {
+        0: createParent({ checked: true }),
+        1: createFirstChild({ checked: true }),
+        2: createGrandChild({ checked: true }),
+        3: createSecondChild({ checked: true })
+      }
+    }
+  ].forEach(({ prevState: state, id, newState: expectedState }) => {
+    describe('Given a state and an id', () => {
+      let newState
+
+      beforeEach(() => {
+        newState = getNewState({ state, id })
+      })
+
+      test('returns a new state', () => {
+        expect(newState).toEqual(expectedState)
+      })
     })
   })
 })
