a047cdc01fbaee002d7a5c7e0cbff4bb15c09abb
Joel George V <joel.gv3@gmail.com>
2020-12-27 17:14:31 +0000

    Use h1 element

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Intro = () => (
   <>
-    <h2>React Checkbox Tree</h2>
+    <h1>React Checkbox Tree</h1>
     <p>
       A react app showcasing a simple checkbox tree component. This project was
       created using{' '}
