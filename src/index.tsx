d9750d8a57b8991b492642abcfd1f1e688ce0298
Joel George V <joel.gv3@gmail.com>
2023-11-19 17:55:29 +0000

    chore: delete woff font

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { createGlobalStyle } from 'styled-components'
 import { App } from './App'
 
 import woff2 from './fonts/source-sans-pro-v11-latin-regular.woff2'
-import woff from './fonts/source-sans-pro-v11-latin-regular.woff'
 
 const GlobalStyle = createGlobalStyle`
   html {
@@ -23,15 +22,15 @@ const GlobalStyle = createGlobalStyle`
     overscroll-behavior-y: none;
   }
 
-  /* source-sans-pro-regular - latin */
   @font-face {
     font-family: 'Source Sans Pro';
     font-style: normal;
     font-weight: 400;
     font-display: optional;
-    src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'),
-        url(${woff2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
-        url(${woff}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+    src:
+      local('Source Sans Pro Regular'),
+      local('SourceSansPro-Regular'),
+      url(${woff2}) format('woff2');
   }
 `
 
