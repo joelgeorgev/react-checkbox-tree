7de7b74a1dc3cc689f9bbd1f2d06aeac495241d7
Joel George V <joel.gv3@gmail.com>
2026-02-23 17:37:26 +0000

    fix: point base to repo name

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import packageJson from './package.json'
+
 export default defineConfig(() => {
   return {
     build: {
+      base: `${new URL(packageJson.repository).pathname}/`,
       outDir: 'build'
     },
     test: {
