a2a49142e1302f05b3839757ccc59dec39e950e9
Joel George V <joel.gv3@gmail.com>
2026-03-03 14:12:10 +0000

    refactor: add gap using css

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,13 +1,18 @@
+import './Intro.css'
+
 export const Intro = () => (
-  <>
+  <div className='intro'>
     <h1>React Checkbox Tree</h1>
     <p>
       A react app showcasing a simple checkbox tree component. This project was
-      created using{' '}
-      <a href='https://github.com/facebookincubator/create-react-app'>
+      created using
+      <a
+        href='https://github.com/facebookincubator/create-react-app'
+        className='link'
+      >
         create-react-app
       </a>
       .
     </p>
-  </>
+  </div>
 )
